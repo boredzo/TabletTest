25ed0989c2762a0a1290f8f49abf3b1ca47b7172
Peter Hosey <git@boredzo.org>
2012-11-14 11:13:39 -0800

    Initial Commit

---
+++ TabletTest/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TabletTest
+//
+//  Created by Peter Hosey on 2012-11-14.
+//  Copyright (c) 2012 Peter Hosey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
