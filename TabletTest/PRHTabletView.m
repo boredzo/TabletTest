099fd8f8cece2365a353827cf5874e884921cf05
Peter Hosey <git@boredzo.org>
2012-11-14 13:26:45 -0800

    Draw a mouse or tablet icon (according to event type) that follows the pen.

--- TabletTest/PRHTabletView.m
+++ TabletTest/PRHTabletView.m
@@ -2,9 +2,48 @@
 
 
 @implementation PRHTabletView
+{
+	NSPoint lastScribbledPoint;
+	NSImage *lastEventTypeIcon;
+	NSImage *eventTypeIconMouse, *eventTypeIconTablet;
+}
 
 - (void) viewDidMoveToWindow {
 	[self.window setAcceptsMouseMovedEvents:YES];
+}
+
+- (bool) eventIsMouseEvent:(NSEvent *)event {
+	NSEventType type = event.type;
+	switch (type) {
+		case NSLeftMouseDown:
+		case NSLeftMouseUp:
+		case NSRightMouseDown:
+		case NSRightMouseUp:
+		case NSMouseMoved:
+		case NSLeftMouseDragged:
+		case NSRightMouseDragged:
+		case NSMouseEntered:
+		case NSMouseExited:
+			return true;
+		default:
+			return false;
+	}
+}
+- (bool) eventIsTabletEvent:(NSEvent *)event {
+	NSEventType type = event.type;
+	switch (type) {
+		case NSTabletPoint:
+		case NSTabletProximity:
+		    return true;
+		default:
+	        return false;
+	}
+}
+
+- (NSRect) rectForImage:(NSImage *)image toBeDrawnCenteredAtPoint:(NSPoint)center {
+	NSSize size = image.size;
+	NSPoint origin = { lastScribbledPoint.x - size.width / 2.0, lastScribbledPoint.y - size.height / 2.0 };
+	return (NSRect){ origin, size };
 }
 
 - (void) reportEvent:(NSEvent *)event {
@@ -23,6 +62,30 @@
 	self.pressureField.floatValue = self.pressureSlider.floatValue = event.tangentialPressure;
 
 	[self.pointingDeviceTypeRadio selectCellAtRow:0 column:event.pointingDeviceType];
+
+	if (!eventTypeIconMouse) {
+		eventTypeIconMouse = [[NSImage alloc] initWithContentsOfFile:@"/System/Library/PreferencePanes/Mouse.prefPane/Contents/Resources/Mouse.icns"];
+		eventTypeIconMouse.size = (NSSize){ 32.0, 32.0 };
+	}
+	if (!eventTypeIconTablet) {
+		eventTypeIconTablet = [[NSImage alloc] initWithContentsOfFile:@"/System/Library/PreferencePanes/Ink.prefPane/Contents/Resources/Ink.icns"];
+		eventTypeIconTablet.size = (NSSize){ 32.0, 32.0 };
+	}
+
+	if (lastEventTypeIcon) {
+		[self setNeedsDisplayInRect:[self rectForImage:lastEventTypeIcon toBeDrawnCenteredAtPoint:lastScribbledPoint]];
+	}
+
+	lastScribbledPoint = [self convertPoint:event.locationInWindow fromView:nil];
+
+	NSImage *newEventTypeIcon = [self eventIsMouseEvent:event] ? eventTypeIconMouse : [self eventIsTabletEvent:event] ? eventTypeIconTablet : nil;
+	if (lastEventTypeIcon != newEventTypeIcon) {
+		lastEventTypeIcon = newEventTypeIcon;
+
+		if (lastEventTypeIcon) {
+			[self setNeedsDisplayInRect:[self rectForImage:lastEventTypeIcon toBeDrawnCenteredAtPoint:lastScribbledPoint]];
+		}
+	}
 }
 
 - (void) tabletPoint:(NSEvent *)event {
@@ -50,6 +113,12 @@
 - (void) mouseUp:(NSEvent *)event {
 	NSLog(@"%s: %hd", __func__, [event subtype]);
 	[self reportEvent:event];
+}
+
+- (void) drawRect:(NSRect)dirtyRect {
+	NSRect destRect = [self rectForImage:lastEventTypeIcon toBeDrawnCenteredAtPoint:lastScribbledPoint];
+	[lastEventTypeIcon drawInRect:destRect fromRect:NSZeroRect
+	                    operation:NSCompositeSourceOver fraction:1.0];
 }
 
 @end
